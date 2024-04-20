6421fddd07a6a32c239d0a6034e82273b5d543f2
Fatih Şahin <94977459+ssahinfatih@users.noreply.github.com>
2024-04-20 12:37:47 +0300

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:`
+    
+    <form [formGroup]="frm" (ngSubmit)="onSubmit()">
+      <input type="text" placeholder="Name" formControlName="name"><br>
+      <input type="text" placeholder="Surname" formControlName="surname"><br>
+      <input type="text" placeholder="E-mail" formControlName="email"><br>
+      <input type="text" placeholder="Tel" formControlName="tel"><br>
+      <div formGroupName="address">
+        <input type="text" placeholder="Country" formControlName="country"><br>
+        <input type="text" placeholder="City" formControlName="city"><br>
+        <input type="text" placeholder="Address" formControlName="city"><br>
+      </div><br>
+      <button>Send</button>
+    </form>
+  `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'proje10-modeldriven';
+
+  frm:FormGroup;
+  constructor(private formBuilder: FormBuilder){
+     this.frm = formBuilder.group({
+      name:["",Validators.required],//validataor requried ile zorunluluk koyuyoruz
+      surname:[""],
+      email:[""],
+      tel:[""],
+      address:formBuilder.group({
+        country:[""],
+        city:[""],
+        address:[""]
+      })
+     })
+    
+     this.frm.valueChanges.subscribe({//formda değişiklik oldukça çalışır
+      next:data => {
+        console.log(data);
+      }
+     })
+  }
+ 
+  onSubmit(){
+    console.log(this.frm.value)
+  }
+
+  
 }
